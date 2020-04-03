59d170a7296c8753ddf3ecf15ebca11f16c3a67c
sAbIz <bellett@sabiz.jp>
2020-04-03 12:17:58 +0900

    add color

--- visualize/_visualize.py
+++ visualize/_visualize.py
@@ -8,7 +8,10 @@ from datit.type import *
 
 __all__ = []
 
-_palette = ['#005CAF', '#F596AA', '#FFB11B', '#BEC23F', '#77428F']
+_palette = [
+        '#005CAF', '#F596AA', '#FFB11B', '#BEC23F', '#77428F',
+        '#E16B8C', '#F05E1C', '#FFC408', '#5DAC81', '#70649A',
+        ]
 
 def _create_fig(**kwargs) -> Plots:
     return plt.subplots(**kwargs)
